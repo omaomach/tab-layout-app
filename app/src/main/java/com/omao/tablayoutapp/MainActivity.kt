b5033255f19948cbed28d428c9f0bfe2e2cb6d3d
omaomach <aaronjoash44@gmail.com>
2023-09-18 17:20:14 +0300

    completed tab layout app

--- app/src/main/java/com/omao/tablayoutapp/MainActivity.kt
+++ app/src/main/java/com/omao/tablayoutapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.omao.tablayoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.omao.tablayoutapp.adapter.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    val tabsArray = arrayOf("Games", "Movies","Books")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+
+        val my_adapter = ViewPagerAdapter(
+            supportFragmentManager,
+            lifecycle
+        )
+
+        viewPager.adapter = my_adapter
+
+        // Connecting the viewpager with tab layout
+        TabLayoutMediator(tabLayout, viewPager) {
+            tab, position -> tab.text = tabsArray[position]
+        }.attach()
+
     }
 }
